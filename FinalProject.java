a817a041571c251d11e68c6404f29ed6105523cc
Alex <connoa11@tcd.ie>
2022-04-07 16:30:52 +0100

    Adding template for final project

---
+++ FinalProject.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FinalProject
+{
+
+	public static void main(String[] args) 
+	{
+
+	
+
+	}
+
+}
