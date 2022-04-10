e4c3508b6b5792f43e2f5ce59925ec269a28cc0f
Alex <connoa11@tcd.ie>
2022-04-10 20:39:35 +0100

    Updating user input code

--- FinalProject.java
+++ FinalProject.java
@@ -145,37 +145,110 @@ public class FinalProject
 		System.out.println("No such file found.");
 	}
 			
-		
-	    
-// GET USER INPUT		
-		// get user to input 2 bus stops
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Please enter 2 bus stops with a space between them (E.g. 34 96).");
-		String inputStops = scanner.nextLine();
-		
-		// converting input to integer array
-		String[] splitInputArray = inputStops.split(" ");
-		int[] inputStopsArr = new int[splitInputArray.length];
-		for(int i = 0; i < splitInputArray.length; i++)
-		{
-			inputStopsArr[i] = Integer.parseInt(splitInputArray[i]);
-		}
-		
-		
-		// find shortest path between he two bus stops
-		// return shortest path, list of stops on the route and the cost(distance)
-		
-		
-		// make graph
-		//EdgeWeightedGraph graph = new EdgeWeightedGraph(numOfvertices, numOfEdges);
-		
-		// create dijkstra class object
-		//DijkstraSP dijkstraClassObj = new DijkstraSP(graph, source);
-		
-		
-
+			
+	boolean quit = false;
+	Scanner scanner = new Scanner(System.in);
+	System.out.println("Welcome to the Vancouver public transport system!");
+	System.out.println("This system allows you to: \n1. Find shortest path between 2 bus stops. \n2. Retrieve all info related to a particular bus stop. \n3. Get details of all trips arriving at specified time. \n");
+	System.out.println("Which option would you like to choose? \nPlease choose by typing 1, 2 or 3. Or type 'quit' if you would like to close the application.\n");
 	
-
+	//do {	
+				
+		 Scanner input = new Scanner(System.in);
+						
+	     if(input.hasNextInt())
+		 {
+		    Integer userChoice = input.nextInt();
+		    
+		    if(userChoice == 1)
+		    {
+				System.out.println("Please enter the bus stop 1\n");
+				Scanner firstStop = new Scanner(System.in);
+				if(firstStop.hasNextLine())
+				{
+					Integer source = firstStop.nextInt();
+								
+					System.out.println("Please enter the bus stop 2\n");
+					Scanner secondStop = new Scanner(System.in);
+					int destination = secondStop.nextInt();
+					
+					DijkstraSP dijkstraObj = new DijkstraSP(graph, source);
+					System.out.print("The shortest path distance between the 2 bus stops is: " +  dijkstraObj.distTo(destination) + "\n");
+					
+					System.out.print("The stops on the shortest route are: " + dijkstraObj.pathTo(destination)+ "\n");
+					firstStop.close();
+					secondStop.close();
+				}
+				else
+				{
+					System.out.println("Bus stop does not exist. Please enter a valid bus stop.");
+				}
+				
+				
+		    }
+		    else if(userChoice == 2)
+		    {
+		    	System.out.print("Please enter bus stops' full name or the first few characters in it's name to retrieve all information relating to stop.\n");
+		    	Scanner userStopInput = new Scanner(System.in);
+		    	//if(userStopInput.hasNextLine())
+		    	//{
+		    		String name = userStopInput.nextLine();
+		    		//if(busStopTST.keysWithPrefix(name) != null)
+		    		//{
+		    		System.out.print(busStopTST.keysWithPrefix(name));
+		    		//}
+		    		//else
+		    		//{
+		    			//System.out.println("Sorry - No bus stops match your search.");
+		    		//}			
+		    		userStopInput.close();
+		    	//}
+		    	//else
+		    	//{
+		    		//System.out.println("Error - please enter letters only");
+		    	//}
+		    }
+		    else if(userChoice == 3)
+		    {
+		    	System.out.print("Please enter bus' arrival time to retrieve all information relating to trip.\n");		   
+		    	Scanner userTimeInput = new Scanner(System.in);
+		    	if(userTimeInput.hasNextLine())
+		    	{
+		    		String time = userTimeInput.nextLine();
+		    		if(busStopTST.keysWithPrefix(time) == null)
+		    		{
+		    			System.out.println("Sorry - No bus stops match your search.");
+		    		}
+		    		else
+		    		{
+		    			System.out.print(arrivalTimeTST.keysWithPrefix(time));
+		    		}			
+		    		
+		    	}
+		    	else
+		    	{
+		    		System.out.println("Error - Please enter a valid arrival time;");
+		    	}
+		    	userTimeInput.close();
+		    }
+		    
+	 }
+	 else if(input.hasNext("quit"))
+	 {
+		  quit = true;
+     }
+	 else
+	 {
+		 System.out.println("Please enter a valid choice number (1-3)\n");
+		 System.out.println("Which option would you like to choose? \nPlease choose by typing 1, 2 or 3. Or type 'quit' if you would like to close the application.\n");
+	 }
+	
+	//} while(!quit);
+	scanner.close();
 	}
-
 }
+	
+	
+		
+
+
